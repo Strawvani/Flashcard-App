1ca4e561cd3b7e540eb63a0be6836498416b302b
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-05-26 16:26:09 +0800

    Created User entity

---
+++ src/main/java/jordan/flashcard_app/Model/User.java
@@ -0,0 +1,28 @@
+package jordan.flashcard_app.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    // constructor
+    public User() {
+    }
+
+    // getters and setters
+
+}
