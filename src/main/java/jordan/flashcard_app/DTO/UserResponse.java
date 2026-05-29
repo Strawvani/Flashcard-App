e025fbc7277f858f3aec757bf76dbdf29ffa77e7
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-05-29 18:19:31 +0800

    Added fromEntity factory method in UserResponse

--- src/main/java/jordan/flashcard_app/DTO/UserResponse.java
+++ src/main/java/jordan/flashcard_app/DTO/UserResponse.java
@@ -1,5 +1,6 @@
 package jordan.flashcard_app.DTO;
 
+import jordan.flashcard_app.Model.User;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,4 +12,12 @@ public class UserResponse {
     private Long id;
     private String username;
     private String email;
+
+    public static UserResponse fromEntity(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .build();
+    }
 }
