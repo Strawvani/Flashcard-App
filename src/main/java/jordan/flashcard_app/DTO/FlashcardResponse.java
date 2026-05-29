1cc7fac139befd0d71073fb96cb0c50416d97265
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-05-29 18:18:05 +0800

    Added fromEntity factory method in FlashcardResponse

--- src/main/java/jordan/flashcard_app/DTO/FlashcardResponse.java
+++ src/main/java/jordan/flashcard_app/DTO/FlashcardResponse.java
@@ -1,5 +1,6 @@
 package jordan.flashcard_app.DTO;
 
+import jordan.flashcard_app.Model.Flashcard;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,4 +14,14 @@ public class FlashcardResponse {
     private String term;
     private String status;
     private String deckName;
+
+    public static FlashcardResponse fromEntity(Flashcard flashcard) {
+        return FlashcardResponse.builder()
+                .id(flashcard.getId())
+                .definition(flashcard.getDefinition())
+                .term(flashcard.getTerm())
+                .status(flashcard.getStatus().getDisplayName())
+                .deckName(flashcard.getDeck().getName())
+                .build();
+    }
 }
